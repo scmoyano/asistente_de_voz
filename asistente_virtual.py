274895028feeb599358389fc54ef14fc86de2c18
scmoyano <sc.moyano90@gmail.com>
2023-02-08 19:14:16 -0300

    retoques

--- asistente_virtual.py
+++ asistente_virtual.py
@@ -88,7 +88,7 @@ def pedir_dia( ):
                  1: "martes",
                  2: "miercoles",                   
                  3: "jueves",
-                 4 :"viernes",
+                 4: "viernes",
                  5: "sabado",
                  6: "domingo"}
     
